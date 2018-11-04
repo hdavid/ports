97af8272546df837bae84eacade8307712c05fe6
Henri David <mots@mots.lan>
2018-11-04 15:17:11 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -4,19 +4,16 @@
 ### generate hostname from mac address
 MAC=`cat /sys/class/net/wlan0/address`
 PORTS_HOSTNAME="ports-${MAC: -2}"
-echo hostname is $PORTS_HOSTNAME
+echo configuring hostname: $PORTS_HOSTNAME
 echo $PORTS_HOSTNAME | sudo tee /etc/hostname
-#sudo sed -i 's/127.0.0.1/127.0.0.1 $PORTS_HOSTNAME/' /etc/hosts 
-#echo "127.0.0.1 $PORTS_HOSTNAME" | sudo tee -a /etc/hosts
 
 ### install dependencies
 echo Install dependencies
 sudo apt-get update > /dev/null && sudo apt-get install -y --force-yes dnsmasq git liblo-dev htop vim > /dev//null
-# prevent dhcp to start at boot
+echo prevent dhcp to start at boot
 sudo update-rc.d -f dnsmasq remove > /dev/null
-
+echo configure Avahi for ports
 sudo cp support/etc_avahi_services_ports.service /etc/avahi/services/ports.service
-sudo /etc/init.d/avahi restart
 
 ### update wifi config to enable fallback on ports ssid 
 if sudo grep -q "mode=2" /etc/wpa_supplicant/wpa_supplicant.conf; then
@@ -27,19 +24,15 @@ else
 
 network={
 	ssid="holon"
-	psk=""
+	psk="XX"
 }
 network={
 	ssid="biomusic"
-	psk=""
-}
-network={
-	ssid="holon"
-	psk=""
+	psk="XX"
 }
 network={
 	ssid="redpill"
-	psk=""
+	psk="XX"
 }
 
 
@@ -116,8 +109,3 @@ g++ \
 	-std=c++0x \
 	-pthread && \
 	sudo mv portsd /usr/sbin
-
-### service script
-#chmod +x support/ports 
-#sudo cp support/ports /etc/init.d/
-#sudo update-rc.d ports defaults
